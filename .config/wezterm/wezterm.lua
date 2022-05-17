583f36672f957e8a77f5609ddd9b0de39a25e330
ghoulboii <socialmedia@ghoulcloud.slmail.me>
2022-05-17 13:29:42 +0530

    Removing emacs

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,21 +1,19 @@
 local wezterm = require 'wezterm'
-wezterm.log_info("Config file " .. wezterm.config_file)
-
 wezterm.on("toggle-opacity", function(window, pane)
  local overrides = window:get_config_overrides() or {}
   if not overrides.window_background_opacity then
-    overrides.window_background_opacity = nil
-  else
     overrides.window_background_opacity = 1.0;
+  else
+    overrides.window_background_opacity = nil
   end
   window:set_config_overrides(overrides)
 end)
 
 return {
+  font = wezterm.font("Hack Nerd Font"),
+  hide_tab_bar_if_only_one_tab = true,
+  window_background_opacity = 0.8,
   keys = {
     {key="B", mods="CTRL", action=wezterm.action{EmitEvent="toggle-opacity"}},
   },
-  font = wezterm.font("Hack Nerd Font"),
-  window_background_opacity = 0.8,
-  hide_tab_bar_if_only_one_tab = true,
 }

f4460d8a4dca06076154ab4ccc44510ee8d9eb83
ghoulboii <socialmedia@ghoulcloud.slmail.me>
2022-05-26 02:32:40 +0530

    Adding cheatsheat

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -42,7 +42,9 @@
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
 
-
+(setq elfeed-feeds
+      '("https://www.reddit.com/r/FreeGameFindings/.rss"
+	"https://barter.vg/u/7a09/o/rss/"))
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
 ;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
 ;;
--- .config/doom/custom.el
+++ .config/doom/custom.el
@@ -3,6 +3,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(elfeed-feeds
+   '("https://barter.vg/u/7a09/o/rss/" "https://www.reddit.com/r/FreeGameFindings/.rss \"FGF\"") t)
+ '(newsticker-url-list
+   '(("FreeGameFindings" "https://www.reddit.com/r/FreeGameFindings/.rss" nil nil nil)
+     ("NBA" "https://www.reddit.com/r/nba/.rss" nil nil nil)
+     ("Github" "https://bandito.re/1b70cdfb-71e0-4863-b394-7169ce97efc0.atom" nil nil nil)))
  '(package-selected-packages '(lsp-javacomp lsp-java))
  '(warning-suppress-types '((hack-local-variables-hook) (hack-local-variables-hook))))
 (custom-set-faces

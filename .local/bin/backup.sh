55ffab74acdde329f568f54408350c9f52d8a1d8
GhoulBoii <github@ghoulcloud.slmail.me>
2024-06-17 01:35:05 +0530

    feat(scripts): update backup

--- .local/bin/backup.sh
+++ .local/bin/backup.sh
@@ -4,6 +4,7 @@ echo -n "Enter external full path to folder to backup to: " || exit 1
 read external_dir
 mkdir -p external_dir
 
+echo "Select gpg key to backup: "
 gpg_id=$(gpg --list-secret-keys --keyid-format LONG | grep sec | awk '{print $2}' | awk -F '/' '{print $2}' | fzf)
 gpg --export-secret-keys $gpg_id > $external_dir/my-private-key.asc
 cp ~/.ssh/id_* $external_dir
